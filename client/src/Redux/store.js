c34a75da35d4c84507a2e4a752c2f49e9ed484f6
Srikar282006 <160750858+Srikar282006@users.noreply.github.com>
2025-04-12 10:12:14 +0530

    Update store.js

--- client/src/Redux/store.js
+++ client/src/Redux/store.js
@@ -3,7 +3,7 @@ import userSlice from "./UserSlice";
 import freelancerSlice from "./FreelancerSlice";
 import clientSlice from "./ClientSlice";
 import chatSlice from "./ChatSlice";
-
+ 
 export const store = configureStore({
   reducer: {
     user: userSlice,
